59bee4861de049104106322e1451564c5e9ae4aa
AGYAPAL SINGH <agyapalsinghaujla@gmail.com>
2025-04-28 22:43:09 +0530

    Header Updated

---
+++ app/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaHeart } from "react-icons/fa";
+const Header = () => {
+  return (
+    <header>
+      <h1>Kakeeeeee !!!!!</h1>
+      <h3>HAPPY BIRTH DAYYYYYY !!!!!</h3>
+
+      <div className="heart1">
+        <FaHeart />
+      </div>
+      <div className="heart2">
+        <FaHeart />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
+import Header from "./Components/Header";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
