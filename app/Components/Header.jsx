1aab5f9f9b5b356d56916104361a201dac07b30c
AGYAPAL SINGH <agyapalsinghaujla@gmail.com>
2025-04-30 17:11:01 +0530

    Header updated

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,16 +1,50 @@
-import React from "react";
-import { FaHeart } from "react-icons/fa";
+"use client";
+import React, { useEffect } from "react";
+import Image from "next/image";
+import gsap from "gsap";
+
 const Header = () => {
+  useEffect(() => {
+    const tl = gsap.timeline({ delay: 6 });
+
+    tl.fromTo(
+      ".char",
+      { y: -100, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.05,
+        duration: 0.6,
+        ease: "back.out(1.7)",
+      }
+    );
+  }, []);
+
+  const splitText = (text) =>
+    Array.from(text).map((char, i) => (
+      <span key={i} className="char">
+        {char}
+      </span>
+    ));
+
   return (
     <header>
-      <h1>Kakeeeeee !!!!!</h1>
-      {/* <h3>HAPPY BIRTH DAYYYYYY !!!!!</h3> */}
+      <div className="ag-header-container">
+        <div className="ag-header-image">
+          <Image
+            src="/images/OG-2.png"
+            alt="OG"
+            width={300}
+            height={450}
+            priority
+          />
+        </div>
 
-      <div className="heart1">
-        <FaHeart />
-      </div>
-      <div className="heart2">
-        <FaHeart />
+        <div className="ag-header-content">
+          <h1>{splitText("Happy 😄")}</h1>
+          <h1 className="ag-header-content-sp">{splitText("Birthday 🎂")}</h1>
+          <h1>{splitText("Kake 👶")}</h1>
+        </div>
       </div>
     </header>
   );
